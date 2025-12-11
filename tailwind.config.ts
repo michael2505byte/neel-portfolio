58485a45716a6f55ce036b37b2f225a9f7656a67
Neel Shah <neelshah644@gmail.com>
2025-12-11 18:45:12 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{ts,tsx,js,jsx}",
+    "./src/components/**/*.{ts,tsx,js,jsx}",
+    "./src/pages/**/*.{ts,tsx,js,jsx}",
+    "./src/data/**/*.{ts,tsx,js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
